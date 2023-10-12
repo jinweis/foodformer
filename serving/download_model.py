aa3c893368dbbe98dc2e12d43738d70da3bb8950
sunshineccl <jinwei.k.sun@gmail.com>
2023-10-12 16:43:18 -0700

    Update download_model.py

--- serving/download_model.py
+++ serving/download_model.py
@@ -1,3 +1,25 @@
+# import os
+# from pathlib import Path
+
+# import wandb
+
+# if not os.environ.get("WANDB_API_KEY"):
+#     raise ValueError(
+#         "You must set the WANDB_API_KEY environment variable " "to download the mod
+# el."
+#     )
+
+# wandb_team = "mlops-usf"
+# wandb_project = "foodformer-serving"
+# wandb_model = "jxkiqv3f"
+# wandb_model_path = f"{wandb_team}/{wandb_project}/{wandb_model}"
+
+# wandb.init()
+
+# current_folder = Path(__file__).parent
+# print(f"Folder: {current_folder}")
+# path = wandb.use_artifact(wandb_model_path).download()
+# print(f"Model downloaded to: {path}")
 import os
 
 import wandb
@@ -10,59 +32,28 @@ if not os.environ.get("WANDB_API_KEY"):
         "You must set the WANDB_API_KEY environment variable " "to download the model."
     )
 
-wandb_team = "jinwei-k-sun"
-wandb_project = "Foodformer"
-wandb_model = "vit:v0"
-wandb_model_path = f"{wandb_team}/{wandb_project}/{wandb_model}"
+entity = "jinwei-k-sun"
+project = "foodformer-serving"
+run_name = "jxkiqv3f"
 
 # Initialize the wandb API
 api = wandb.Api()
 
 # Get all runs from the project
-runs = api.runs(f"{wandb_team}/{wandb_project}")
+runs = api.runs(f"{entity}/{project}")
 
 # Find the run ID for the specified run name
-for run in wandb_model:
-    if run.name == wandb_model:
+for run in runs:
+    if run.name == run_name:
         run_id = run.id
         break
 else:
-    raise ValueError(
-        f"Run with name {wandb_model} not found in project {wandb_project}"
-    )
+    raise ValueError(f"Run with name {run_name} not found in project {project}")
 
-run = api.run(f"{wandb_team}/{wandb_project}/{run_id}")
-run.file(wandb_model).download(replace=True)
+# Now you can use the run ID to get the specific run and download the file
+file_name = "best_model.ckpt"
+run = api.run(f"{entity}/{project}/{run_id}")
+run.file(file_name).download(replace=True)
+
 
 print("Model downloaded")
-# current_folder = Path(__file__).parent
-# print(f"Folder: {current_folder}")
-# path = wandb.use_artifact(wandb_model_path).download()
-# print(f"Model downloaded to: {path}")
-
-
-# entity = "wei_academic"
-# project = "Foodformer_res"
-# run_name = "VisionTransformer-5epochs"
-
-# # Initialize the wandb API
-# api = wandb.Api()
-
-# # Get all runs from the project
-# runs = api.runs(f"{entity}/{project}")
-
-# # Find the run ID for the specified run name
-# for run in runs:
-#     if run.name == run_name:
-#         run_id = run.id
-#         break
-# else:
-#     raise ValueError(f"Run with name {run_name} not found in project {project}")
-
-# # Now you can use the run ID to get the specific run and download the file
-# file_name = "best_model.ckpt"
-# run = api.run(f"{entity}/{project}/{run_id}")
-# run.file(file_name).download(replace=True)
-
-
-# print(f"Model downloaded")
