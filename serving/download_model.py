44c9d0524bbfc9b56c07ebc72f1718a82185d451
sunshineccl <jinwei.k.sun@gmail.com>
2023-10-12 16:27:42 -0700

    Update download_model.py

--- serving/download_model.py
+++ serving/download_model.py
@@ -1,7 +1,9 @@
 import os
-from pathlib import Path
 
 import wandb
+
+# from pathlib import Path
+
 
 if not os.environ.get("WANDB_API_KEY"):
     raise ValueError(
@@ -13,9 +15,54 @@ wandb_project = "Foodformer"
 wandb_model = "vit:v0"
 wandb_model_path = f"{wandb_team}/{wandb_project}/{wandb_model}"
 
-wandb.init()
+# Initialize the wandb API
+api = wandb.Api()
 
-current_folder = Path(__file__).parent
-print(f"Folder: {current_folder}")
-path = wandb.use_artifact(wandb_model_path).download()
-print(f"Model downloaded to: {path}")
+# Get all runs from the project
+runs = api.runs(f"{wandb_team}/{wandb_project}")
+
+# Find the run ID for the specified run name
+for run in wandb_model:
+    if run.name == wandb_model:
+        run_id = run.id
+        break
+else:
+    raise ValueError(
+        f"Run with name {wandb_model} not found in project {wandb_project}"
+    )
+
+run = api.run(f"{wandb_team}/{wandb_project}/{run_id}")
+run.file(wandb_model).download(replace=True)
+
+print("Model downloaded")
+# current_folder = Path(__file__).parent
+# print(f"Folder: {current_folder}")
+# path = wandb.use_artifact(wandb_model_path).download()
+# print(f"Model downloaded to: {path}")
+
+
+# entity = "wei_academic"
+# project = "Foodformer_res"
+# run_name = "VisionTransformer-5epochs"
+
+# # Initialize the wandb API
+# api = wandb.Api()
+
+# # Get all runs from the project
+# runs = api.runs(f"{entity}/{project}")
+
+# # Find the run ID for the specified run name
+# for run in runs:
+#     if run.name == run_name:
+#         run_id = run.id
+#         break
+# else:
+#     raise ValueError(f"Run with name {run_name} not found in project {project}")
+
+# # Now you can use the run ID to get the specific run and download the file
+# file_name = "best_model.ckpt"
+# run = api.run(f"{entity}/{project}/{run_id}")
+# run.file(file_name).download(replace=True)
+
+
+# print(f"Model downloaded")
